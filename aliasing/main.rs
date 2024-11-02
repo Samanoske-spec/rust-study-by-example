8f1a884e787fce5ee97f5aef5447acf3e7d94859
Samanoske-spec <louisemily640@gmail.com>
2024-11-02 12:32:09 +1000

    new concept

--- aliasing/main.rs
+++ aliasing/main.rs
@@ -1,17 +1,65 @@
-// `NanoSecond`, `Inch`, and `U64` are new names for `u64`.
-type NanoSecond = u64;
-type Inch = u64;
-type U64 = u64;
+use std::mem;
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy)]
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+// A Rectangle can be specified by where its top left and bottom right 
+// corners are in space
+#[allow(dead_code)]
+struct Rectangle {
+    top_left: Point,
+    bottom_right: Point,
+}
+
+fn origin() -> Point {
+    Point { x: 0.0, y: 0.0 }
+}
+
+fn boxed_origin() -> Box<Point> {
+    // Allocate this point on the heap, and return a pointer to it
+    Box::new(Point { x: 0.0, y: 0.0 })
+}
 
 fn main() {
-    // `NanoSecond` = `Inch` = `U64` = `u64`.
-    let nanoseconds: NanoSecond = 5 as u64;
-    let inches: Inch = 2 as U64;
+    // (all the type annotations are superfluous)
+    // Stack allocated variables
+    let point: Point = origin();
+    let rectangle: Rectangle = Rectangle {
+        top_left: origin(),
+        bottom_right: Point { x: 3.0, y: -4.0 }
+    };
 
-    // Note that type aliases *don't* provide any extra type safety, because
-    // aliases are *not* new types
-    println!("{} nanoseconds + {} inches = {} unit?",
-             nanoseconds,
-             inches,
-             nanoseconds + inches);
+    // Heap allocated rectangle
+    let boxed_rectangle: Box<Rectangle> = Box::new(Rectangle {
+        top_left: origin(),
+        bottom_right: Point { x: 3.0, y: -4.0 },
+    });
+
+    // The output of functions can be boxed
+    let boxed_point: Box<Point> = Box::new(origin());
+
+    // Double indirection
+    let box_in_a_box: Box<Box<Point>> = Box::new(boxed_origin());
+
+    println!("Point occupies {} bytes on the stack",
+             mem::size_of_val(&point));
+    println!("Rectangle occupies {} bytes on the stack",
+             mem::size_of_val(&rectangle));
+
+    // box size == pointer size
+    println!("Boxed point occupies {} bytes on the stack",
+             mem::size_of_val(&boxed_point));
+    println!("Boxed rectangle occupies {} bytes on the stack",
+             mem::size_of_val(&boxed_rectangle));
+    println!("Boxed box occupies {} bytes on the stack",
+             mem::size_of_val(&box_in_a_box));
+
+    // Copy the data contained in `boxed_point` into `unboxed_point`
+    let unboxed_point: Point = *boxed_point;
+    println!("Unboxed point occupies {} bytes on the stack",
+             mem::size_of_val(&unboxed_point));
 }
